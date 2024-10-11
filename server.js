7441a9acb54a64dad217943100de594c40bc1fb8
Matyáš Kumžák <134993015+qwertyczee@users.noreply.github.com>
2024-10-11 09:47:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { YoutubeTranscript } = require('youtube-transcript');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
