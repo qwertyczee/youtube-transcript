44bacd30b0ecd6cc605d3d8e7ec3fa8943a1607e
Matyáš Kumžák <134993015+qwertyczee@users.noreply.github.com>
2024-10-11 11:17:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,7 @@
-const express = require('express');
-const { YoutubeTranscript } = require('youtube-transcript');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = require('./src/app');
 
-app.use(express.json());
+const PORT = 5000;
 
-app.get('/transcript/string/:id', (req, res) => {
-    const videoId = req.params.id;
-
-    YoutubeTranscript.fetchTranscript(videoId)
-        .then(transcript => {
-            res.json(transcript);
-        })
-        .catch(error => {
-            res.status(500).json({ error: 'Error fetching transcript' });
-        });
-});
-
-app.get('/transcript/plaintext/:id', (req, res) => {
-    const videoId = req.params.id;
-
-    YoutubeTranscript.fetchTranscript(videoId)
-        .then(transcript => {
-            res.json(transcript.map(t => t.text).join(' '));
-        })
-        .catch(error => {
-            res.status(500).json({ error: 'Error fetching transcript' });
-        });
-});
-
-app.get('/transcript/timestamps/:id', (req, res) => {
-    const videoId = req.params.id;
-
-    YoutubeTranscript.fetchTranscript(videoId)
-        .then(transcript => {
-            const formattedTranscript = transcript.map(t => `[${t.offset}] ${t.text}`).join('\n');
-            res.send(formattedTranscript);
-        })
-        .catch(error => {
-            res.status(500).json({ error: 'Error fetching transcript' });
-        });
-});
-
-// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
